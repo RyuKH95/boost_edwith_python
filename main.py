2ce006eb1f10121831a5689d8d187939d16ea950
GH Ryu <gh9503@gmail.com>
2020-07-02 19:09:05 +0900

    https://www.edwith.org/aipython/lecture/22952/ Split&Join

--- main.py
+++ main.py
@@ -1,14 +1,34 @@
-# 일반 코드
-colors = ["a", "b", "c", "d", "e"]
-result = ""
+# # 일반 코드
+# colors = ["a", "b", "c", "d", "e"]
+# result = ""
 
-for s in colors:
-    result += s
+# for s in colors:
+#     result += s
 
-print(result)
+# print(result)
 
-# pythonic code
+# # pythonic code
+# colors = ["red", "blue", "green", "yellow"]
+# result = "".join(colors)
+
+# print(result)
+
+# split
+items = 'zero one two three'.split()
+print(items)
+
+example = 'python,jquery,javascript'
+print(example.split(","))
+a, b, c = example.split(",")  # unpacking
+print(a, b, c)
+
+# join
 colors = ["red", "blue", "green", "yellow"]
-result = "".join(colors)
-
+result = ''.join(colors)
+print(result)
+result = ' '.join(colors)
+print(result)
+result = ', '.join(colors)
+print(result)
+result = '-'.join(colors)
 print(result)
